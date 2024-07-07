74e4a957b3e4e458f32ad53a86995cb57c6fe75d
Kavyansh <kd.kavyansh2003@gmail.com>
2024-07-08 04:41:13 +0530

    Updated FileOperations.h, shows route in terminal

--- FileOperations.h
+++ FileOperations.h
@@ -13,144 +13,132 @@
 using namespace std;
 
 vector<Location*> locationParser(string filename, vector<Route*> routes){
-	fstream locations(filename.c_str());
+    fstream locations(filename.c_str());
 
-	string country;
-	string city;
-	string latitude;
-	string longitude;
+    string country;
+    string city;
+    string latitude;
+    string longitude;
 
-	vector<Location*> cities;
-	Location* node;
+    vector<Location*> cities;
+    Location* node;
 
-	while(locations.good()){
-		getline(locations, country, ',');
-		getline(locations, city, ',');
-		getline(locations, latitude, ',');
-		getline(locations, longitude);
-		//cout << "Country:" << country << " City:" << city << " Lat:" << latitude << " Lon:" << longitude << endl << endl << endl;
+    while(locations.good()){
+        getline(locations, country, ',');
+        getline(locations, city, ',');
+        getline(locations, latitude, ',');
+        getline(locations, longitude);
 
-		node = new Location(country, city, atof(latitude.c_str()), atof(longitude.c_str()));
+        node = new Location(country, city, atof(latitude.c_str()), atof(longitude.c_str()));
 
-		vector<Route*>::iterator it = routes.begin();
+        vector<Route*>::iterator it = routes.begin();
 
-		while(it != routes.end()){
-			if((*it) -> originS.compare(node -> capital) == 0){
-				(*it) -> origin = node;
-				node -> routes.push_back((*it));
-			}
-			else if((*it) -> destinationS.compare(node -> capital) == 0){
-				(*it) -> destination = node;
-			}
-			++it;
-		}
+        while(it != routes.end()){
+            if((*it)->originS.compare(node->capital) == 0){
+                (*it)->origin = node;
+                node->routes.push_back((*it));
+            }
+            else if((*it)->destinationS.compare(node->capital) == 0){
+                (*it)->destination = node;
+            }
+            ++it;
+        }
 
-		cities.push_back(node);
-
-	}
-	cout << "Cities Parsed from: " << filename << endl;
-	return cities;
+        cities.push_back(node);
+    }
+    cout << "Cities Parsed from: " << filename << endl;
+    return cities;
 }
-
-
-/*
-Parses and builds Route objects from file routes.csv
-*/
 
 vector<Route*> routeParser(string filename){
+    fstream routes(filename.c_str());
 
-	fstream routes(filename.c_str());
+    string originS;
+    string destinationS;
 
-	string originS;
-	string destinationS;
+    Location* origin = new Location();
+    Location* destination = new Location();
 
-	Location* origin = new Location();
-	Location* destination = new Location();
+    string type;
+    string time;
+    string cost;
+    string note;
 
-	string type;
-	string time;
-	string cost;
-	string note;
+    vector<Route*> allRoutes;
+    Route* edge;
 
-	vector<Route*> allRoutes;
-	Route* edge;
+    while(routes.good()){
+        getline(routes, originS, ',');
+        getline(routes, destinationS, ',');
+        getline(routes, type, ',');
+        getline(routes, time, ',');
+        getline(routes, cost, ',');
+        getline(routes, note);
 
-	while(routes.good()){
-		getline(routes, originS, ',');
-		getline(routes, destinationS, ',');
-		getline(routes, type, ',');
-		getline(routes, time, ',');
-		getline(routes, cost, ',');
-		getline(routes, note);
+        edge = new Route(origin, destination, type, atof(time.c_str()), atof(cost.c_str()), note);
+        edge->destinationS = destinationS;
+        edge->originS = originS;
 
-		//cout << "Origin: " << originS << " Destination: " << destinationS << "---" << type << " " << time << " " << cost << " " << endl; //debug
+        allRoutes.push_back(edge);
+    }
 
-		edge = new Route(origin, destination, type, atof(time.c_str()), atof(cost.c_str()), note);
-		edge -> destinationS = destinationS;
-		edge -> originS = originS;
-
-		allRoutes.push_back(edge);
-	}
-
-	cout << "Routes Parsed from: " << filename << endl;
-	return allRoutes;
+    cout << "Routes Parsed from: " << filename << endl;
+    return allRoutes;
 }
 
-
-
-/*
-Generates an appropriately formatted html file that displays the route information passed in by the stack fo cities and routes
-*/
 void outputGenerator(string filename, stack<Location*> cities, stack<Route*> routes, bool costOrTime){
-	
-	ofstream output(filename.c_str());
-	output << "<HTML><HEAD><TITLE>Shortest path from Italy to Kazakhstan</TITLE></HEAD><script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false'></script><script>function initialize() { var myOptions = { zoom: 3, center: new google.maps.LatLng(0, 0), mapTypeId: google.maps.MapTypeId.ROADMAP};var map=new google.maps.Map(document.getElementById('map'), myOptions);\n";
+    ofstream output(filename.c_str());
+    output << "<HTML><HEAD><TITLE>Shortest path from Italy to Kazakhstan</TITLE></HEAD><script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false'></script><script>function initialize() { var myOptions = { zoom: 3, center: new google.maps.LatLng(0, 0), mapTypeId: google.maps.MapTypeId.ROADMAP};var map=new google.maps.Map(document.getElementById('map'), myOptions);\n";
 
-	int markerCount = 0;
-	int contentStringCount = 0;
-	Location* origin;
-	Location* destination;
-	Route* route;
+    int markerCount = 0;
+    int contentStringCount = 0;
+    Location* origin;
+    Location* destination;
+    Route* route;
 
-	float cost;
+    float totalCost = 0;
+    float totalTime = 0;
 
-	
+    cout << "Travel Route:" << endl;
 
-	while(!cities.empty() && !routes.empty()){
+    while(!cities.empty() && !routes.empty()){
+        origin = cities.top();
+        cities.pop();
+        destination = cities.top();
+                        
+        output << "var marker" << markerCount << " = new google.maps.Marker({ position: new google.maps.LatLng(" << origin->lat << ", " << origin->lon << "), map: map, title: \"" << origin->capital << ", " << origin->country << "\"});\n";
+        
+        markerCount++;
+            
+        output << "var marker" << markerCount << " = new google.maps.Marker({ position: new google.maps.LatLng(" << destination->lat << ", " << destination->lon << "), map: map, title: \"" << destination->capital << ", " << destination->country << "\"});\n";
+        
+        markerCount++;
 
-		//cout << routes.size() << endl << cities.size() << "--" << endl;		//debug
+        route = routes.top();
+        routes.pop();
 
-		origin = cities.top();
-		cities.pop();
-		destination = cities.top();
-						
-		output << "var marker" << markerCount << " = new google.maps.Marker({ position: new google.maps.LatLng(" << origin -> lat << ", " << origin -> lon << "), map: map, title: \"" << origin -> capital << ", " << origin -> country << "\"});\n";
-		
-		markerCount++;
-			
-		output << "var marker" << markerCount << " = new google.maps.Marker({ position: new google.maps.LatLng(" << destination -> lat << ", " << destination -> lon << "), map: map, title: \"" << destination -> capital << ", " << destination -> country << "\"});\n";
-		
-		markerCount++;
+        float cost = route->cost;
+        if(route->transport.compare("plane") == 0){
+            cost = cost / MULTI;
+        }
 
-		route = routes.top();
-		routes.pop();
+        totalCost += cost;
+        totalTime += route->time;
 
-		cost = route -> cost;
-		if(route -> transport.compare("plane") == 0){
-			cost =  cost / MULTI;
-		}
+        cout << origin->capital << ", " << origin->country << " --> " << destination->capital << ", " << destination->country;
+        cout << " (" << route->transport << " - " << route->time << " hours - $" << cost << ")" << endl;
 
-		output << "var contentString" << contentStringCount << " = \"" << origin -> capital << ", " << origin -> country << " --> " << destination -> capital << ", " << destination -> country << "(" << route -> transport << " - " << route -> time << " hours - $" << cost << ")\"; var path" << contentStringCount << " = new google.maps.Polyline({ path: [new google.maps.LatLng(" << origin -> lat << ", " << origin -> lon << "), new google.maps.LatLng(" << destination -> lat << ", " << destination -> lon << ")], strokeColor: '#0000FF', strokeOpacity: 1.0, strokeWeight: 2}); path"<< contentStringCount <<".setMap(map); google.maps.event.addListener(path" << contentStringCount << ", 'click', function(event) { alert(contentString" << contentStringCount << ");});\n";
+        output << "var contentString" << contentStringCount << " = \"" << origin->capital << ", " << origin->country << " --> " << destination->capital << ", " << destination->country << "(" << route->transport << " - " << route->time << " hours - $" << cost << ")\"; var path" << contentStringCount << " = new google.maps.Polyline({ path: [new google.maps.LatLng(" << origin->lat << ", " << origin->lon << "), new google.maps.LatLng(" << destination->lat << ", " << destination->lon << ")], strokeColor: '#0000FF', strokeOpacity: 1.0, strokeWeight: 2}); path" << contentStringCount << ".setMap(map); google.maps.event.addListener(path" << contentStringCount << ", 'click', function(event) { alert(contentString" << contentStringCount << ");});\n";
 
-		contentStringCount++;
-		
-	}
+        contentStringCount++;
+    }
 
-	output << "} google.maps.event.addDomListener(window, 'load', initialize); </script></HEAD><BODY><div id='map' style='width:100%;height:100%;'></div></BODY></HTML>";
-	output.close();
+    output << "} google.maps.event.addDomListener(window, 'load', initialize); </script></HEAD><BODY><div id='map' style='width:100%;height:100%;'></div></BODY></HTML>";
+    output.close();
 
-	cout << "Output File Generated: " << filename << endl;
+    cout << "Total Cost: $" << totalCost << endl;
+    cout << "Total Duration: " << totalTime << " hours" << endl;
+    cout << "Output File Generated: " << filename << endl;
 }
-
 
 #endif
